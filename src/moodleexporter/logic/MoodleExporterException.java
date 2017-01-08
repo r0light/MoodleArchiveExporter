ae1f0d58fdeefed37d04e6bc52bdbf959a2a065d
Robin Lichtenthaeler <robin.lichtenthaeler@stud.uni-bamberg.de>
2017-01-08 11:45:20 +0100

    removed TODOs

--- src/moodleexporter/logic/MoodleExporterException.java
+++ src/moodleexporter/logic/MoodleExporterException.java
@@ -6,27 +6,22 @@ public class MoodleExporterException extends Exception {
 
 	public MoodleExporterException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public MoodleExporterException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
 	}
 
 	public MoodleExporterException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 	public MoodleExporterException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public MoodleExporterException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 }
